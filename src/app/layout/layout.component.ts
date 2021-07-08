4e5e5b028a7728e3a9c7dd9e5aa9cc595613e801
CeAlCuadrado <cealcuadrado@outlook.com>
2021-07-08 17:30:29 -0400

    Agregado contenido al layout

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.scss']
+  styleUrls: ['./layout.component.scss'],
 })
 export class LayoutComponent implements OnInit {
+  nivel: number = 1;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  definirNivelTamannio(): string {
+    return `nivel-${this.nivel}`;
   }
-
 }
