ce19ee380f7a4c18de4cc2ce5020cb1f2e14297a
CeAlCuadrado <cealcuadrado@outlook.com>
2021-07-08 16:51:12 -0400

    Instaladas dependencias, creado layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgxPrettyCheckboxModule } from 'ngx-pretty-checkbox';
 
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './layout/layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgxPrettyCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
